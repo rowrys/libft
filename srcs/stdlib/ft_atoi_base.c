11d6d0c79410a600538364e8c49d1200d5bc4e0a
lasere77 <colin.mewen@gmail.com>
2026-06-03 18:01:54 +0200

    fix(stdlib, atoi_base): UB.

--- srcs/stdlib/ft_atoi_base.c
+++ srcs/stdlib/ft_atoi_base.c
@@ -19,20 +19,18 @@
 
 static inline bool	is_invalid_char(const char c)
 {
-	return (c == '+' || c == '-' || (9 <= c && c <= 13));
+	return (c == '+' || c == '-' || (unsigned char)(c) - 9 <= 13);
 }
 
-static bool	init_buffer(unsigned char *buffer, char const *base)
+static bool	init_buffer(unsigned char *buffer, unsigned char const *base)
 {
 	size_t	i;
 
-	i = 0;
-	while (i < BUFFER_SIZE + 1)
-		buffer[i++] = 255;
+	ft_bzero(buffer, BUFFER_SIZE + 1);
 	i = 0;
 	while (base[i])
 	{
-		if (buffer[(int)base[i]] != 255 || is_invalid_char(base[i]))
+		if (buffer[(int)base[i]] != 0 || is_invalid_char(base[i]))
 			return (1);
 		buffer[(int)base[i]] = i;
 		++i;
@@ -40,30 +38,43 @@ static bool	init_buffer(unsigned char *buffer, char const *base)
 	return (0);
 }
 
+// thanks norm...
+static char const	*get_sign(char const *str, int8_t *sign)
+{
+	while (*str == ' ' || (unsigned char)(*str) - 9 <= 13)
+		++str;
+	*sign = 1;
+	if (*str == '-')
+		*sign = -(*sign);
+	if (*str == '+' || *str == '-')
+		++str;
+	return (str);
+}
+
 int	ft_atoi_base(char const *str, char const *base, bool *of_flag)
 {
 	size_t			i;
-	size_t			base_len;
 	int				result;
+	size_t			base_len;
 	int8_t			sign;
 	unsigned char	buffer[BUFFER_SIZE + 1];
 
-	if (init_buffer(buffer, base))
+	if (init_buffer(buffer, (unsigned char *)base))
 		return (0);
-	while (*str == ' ' || (unsigned char)(*str) - 9 <= 13)
-		++str;
-	sign = 1 - (2 * (*str == '-'));
-	if (*str == '+' || *str == '-')
-		++str;
+	str = get_sign(str, &sign);
+	result = 0;
 	i = 0;
 	base_len = ft_strlen(base);
 	if (of_flag)
 		*of_flag = 0;
 	while (str[i])
 	{
-		if (of_flag && result > (int)(result * base_len) + buffer[(int)str[i]])
+		if (!buffer[(int)(unsigned char)str[i]])
+			return (result * sign);
+		if (of_flag && result > (int)(result * base_len)
+			+ buffer[(int)(unsigned char)str[i]])
 			*of_flag = 1;
-		result = (result * base_len) + buffer[(int)str[i++]];
+		result = (result * base_len) + buffer[(int)(unsigned char)str[i++]];
 	}
 	return (result * sign);
 }
