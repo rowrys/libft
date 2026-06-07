a26e9330cc37a8d8189f7bbb4ccee22f3bf94041
rowrys <colin.mewen@gmail.com>
2026-06-07 14:26:57 +0200

    perf(mem): optimise memset.

--- srcs/mem/ft_memset.c
+++ srcs/mem/ft_memset.c
@@ -10,11 +10,33 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
+#include <stddef.h>
 
 void	*ft_memset(void *s, int c, size_t n)
 {
-	while (n--)
-		((unsigned char *)s)[n] = c;
+	size_t	iter;
+	size_t	j;
+	size_t	big_c;
+	size_t	new_addrs;
+
+	iter = 0;
+	while (((size_t)s + iter) & (sizeof(size_t) - 1) && iter < n)
+		((char *)s)[iter++] = c;
+	j = sizeof(size_t) - 1;
+	big_c = (unsigned char)c;
+	while (j)
+	{
+		big_c <<= 8;
+		big_c |= (unsigned char)c;
+		j--;
+	}
+	while (iter + sizeof(size_t) - 1 < n)
+	{
+		new_addrs = (size_t)s + iter;
+		*((size_t *)new_addrs) = big_c;
+		iter += sizeof(size_t);
+	}
+	while (iter < n)
+		((char *)s)[iter++] = c;
 	return (s);
 }
